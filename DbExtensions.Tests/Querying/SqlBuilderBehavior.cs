2d28107936f9c7fc4dbc56a811bb8a99a2dfb154
colin-young <colin_young@yahoo.com>
2013-12-11 09:44:10 -0500

    Added additional tests
    Verify single parameter case, added test for string arrays. Changed
    arrays to match sample from wiki.

--- DbExtensions.Tests/Querying/SqlBuilderBehavior.cs
+++ DbExtensions.Tests/Querying/SqlBuilderBehavior.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Common;
 
 namespace DbExtensions.Tests.Querying {
-   
+
    [TestClass]
    public class SqlBuilderBehavior {
+      DbConnection conn;
+
+      [TestInitialize]
+      public void Initialize() {
+
+         this.conn = Database.GetProviderFactory("System.Data.SqlClient")
+            .CreateConnection();
+      }
 
       [TestMethod]
       public void Use_Parameter_On_Limit_Clause() {
@@ -30,13 +39,58 @@ namespace DbExtensions.Tests.Querying {
       }
 
       [TestMethod]
-      public void VerifyArrayToList()
-      {
-          var query = SQL
-              .SELECT("*")
-              .WHERE("VALUE IN ({0})", new object[] { "a", "b", "c" });
+      public void VerifySingleValue() {
+         int id = 1;
 
-          Assert.AreEqual(3, query.ParameterValues.Count);
+         var query = SQL
+             .SELECT("*")
+             .WHERE("VALUE = {0}", id);
+
+         Assert.AreEqual(1, query.ParameterValues.Count);
+
+         var cmd = query.ToCommand(conn);
+         Assert.IsTrue(cmd.CommandText.Contains("@p0"));
+         Assert.IsFalse(cmd.CommandText.Contains("@p1"));
+      }
+
+      [TestMethod]
+      public void VerifyArrayToList() {
+         int[] ids = { 1, 2, 3 };
+
+         var query = SQL
+             .SELECT("*")
+             .WHERE("VALUE IN ({0})", ids);
+
+         Assert.AreEqual(3, query.ParameterValues.Count);
+
+         var cmd = query.ToCommand(conn);
+         Assert.IsTrue(cmd.CommandText.Contains("@p2"));
+      }
+
+      [TestMethod]
+      public void VerifyArrayToList2() {
+         var query = SQL
+             .SELECT("*")
+             .WHERE("VALUE IN ({0})", 1, 2, 3);
+
+         Assert.AreEqual(3, query.ParameterValues.Count);
+
+         var cmd = query.ToCommand(conn);
+         Assert.IsTrue(cmd.CommandText.Contains("@p2"));
+      }
+
+      [TestMethod]
+      public void VerifyStringArrayToList() {
+         string[] ids = { "a", "b", "c" };
+
+         var query = SQL
+             .SELECT("*")
+             .WHERE("VALUE IN ({0})", ids);
+
+         Assert.AreEqual(3, query.ParameterValues.Count);
+
+         var cmd = query.ToCommand(conn);
+         Assert.IsTrue(cmd.CommandText.Contains("@p2"));
       }
    }
 }
