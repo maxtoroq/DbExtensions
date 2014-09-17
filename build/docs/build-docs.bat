f7c9ba9adea384800bc7afd5c88654fd8662877f
Max Toro <maxtoroq@gmail.com>
2014-09-17 00:04:20 -0300

    fully-qualified paths in build-docs.bat
    allows you to run it from a different directory

--- build/docs/build-docs.bat
+++ build/docs/build-docs.bat
@@ -1,5 +1,5 @@
-MSBuild.exe ..\..\src\DbExtensions\DbExtensions.csproj /p:Configuration=Release
-MSBuild.exe DbExtensions.shfbproj
-MSBuild.exe ..\..\submodules\sandcastle-md\sandcastle-md.sln
-rd /s /q ..\..\docs\api
-..\..\submodules\sandcastle-md\src\sandcastle-md\bin\Debug\sandcastle-md.exe output ..\..\docs\api
+MSBuild.exe %~dp0..\..\src\DbExtensions\DbExtensions.csproj /p:Configuration=Release
+MSBuild.exe %~dp0DbExtensions.shfbproj
+MSBuild.exe %~dp0..\..\submodules\sandcastle-md\sandcastle-md.sln
+rd /s /q %~dp0..\..\docs\api
+%~dp0..\..\submodules\sandcastle-md\src\sandcastle-md\bin\Debug\sandcastle-md.exe %~dp0output %~dp0..\..\docs\api
