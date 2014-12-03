ad743674013eb075b3c8bb7ac2d34a80114e6785
Max Toro <maxtoroq@gmail.com>
2014-12-03 19:14:23 -0300

    build-docs powershell script

--- build/docs/build-docs.bat
+++
@@ -1,7 +0,0 @@
-cd %~dp0
-
-MSBuild.exe ..\..\src\DbExtensions\DbExtensions.csproj /p:Configuration=Release
-MSBuild.exe DbExtensions.shfbproj
-MSBuild.exe sandcastle-md\sandcastle-md.sln
-rd /s /q ..\..\docs\api
-sandcastle-md\src\sandcastle-md\bin\Debug\sandcastle-md.exe output ..\..\docs\api
