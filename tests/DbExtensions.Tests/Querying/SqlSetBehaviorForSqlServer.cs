74e9c643381eed3dceb709e32b59e5b302cdd4bd
Max Toro <maxtoroq@gmail.com>
2015-02-06 19:17:59 -0300

    test for #33

--- tests/DbExtensions.Tests/Querying/SqlSetBehaviorForSqlServer.cs
+++ tests/DbExtensions.Tests/Querying/SqlSetBehaviorForSqlServer.cs
@@ -39,5 +39,47 @@ namespace DbExtensions.Tests.Querying {
 
          Assert.AreEqual(2, query.ParameterValues.Count);
       }
+
+      [TestMethod]
+      public void Pagination_Sql2008() {
+
+         Database db = new SqlSetForSqlServer.Sql2008Database();
+
+         SqlSet set = db.From("products")
+            .Where("UnitsInStock > 0")
+            .OrderBy("ProductID")
+            .Skip(0)
+            .Take(5)
+            .Select("ProductName");
+
+         SqlBuilder expected = SQL
+            .SELECT("ProductName")
+            .FROM(SQL
+               .SELECT("ROW_NUMBER() OVER (ORDER BY ProductID) AS dbex_rn, __rn.*")
+               .FROM(SQL
+                  .SELECT("*")
+                  .FROM("products"), "__rn")
+               .WHERE("UnitsInStock > 0"), "_")
+            .WHERE("dbex_rn BETWEEN {0} AND {1}", 1, 5)
+            .ORDER_BY("dbex_rn");
+
+         Assert.IsTrue(SqlEquals(set, expected));
+      }
+
+      bool SqlEquals(SqlSet set, SqlBuilder query) {
+         return TestUtil.SqlEquals(set, query);
+      }
+   }
+}
+
+namespace DbExtensions.Tests.Querying.SqlSetForSqlServer {
+   using System.Data.Linq.Mapping;
+   using System.Data.Linq.SqlClient;
+
+   [Provider(typeof(Sql2008Provider))]
+   class Sql2008Database : Database {
+
+      public Sql2008Database()
+         : base(System.Data.SqlClient.SqlClientFactory.Instance.CreateSqlServerConnectionForTests()) { }
    }
 }
