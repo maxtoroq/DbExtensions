9c218e0b264d4a7ee84bd487560a5cec98e4893e
Max Toro <maxtoroq@gmail.com>
2015-02-06 00:33:56 -0300

    test for #35

---
+++ tests/DbExtensions.Tests/Querying/SqlSetBehaviorForOracle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbExtensions.Tests.Querying {
+   
+   [TestClass]
+   public class SqlSetBehaviorForOracle {
+
+      readonly Database db = new Database(Database.GetProviderFactory("System.Data.OracleClient").CreateConnection());
+
+      [TestMethod]
+      public void Apply_Select_Last() {
+
+         SqlSet set = db.From("products")
+            .Where("UnitsInStock > 0")
+            .Take(5)
+            .Select("ProductName");
+
+         SqlBuilder expected = SQL
+            .SELECT("ProductName")
+            .FROM(SQL
+               .SELECT("ROWNUM AS dbex_rn, __1.*")
+               .FROM(SQL
+                  .SELECT("*")
+                  .FROM("products"), "__1")
+               .WHERE("UnitsInStock > 0"), "_")
+            .WHERE("dbex_rn BETWEEN {0} AND {1}", 1, 5)
+            .ORDER_BY("dbex_rn");
+
+         Assert.IsTrue(SqlEquals(set, expected));
+      }
+
+      bool SqlEquals(SqlSet set, SqlBuilder query) {
+         return TestUtil.SqlEquals(set, query);
+      }
+   }
+}
