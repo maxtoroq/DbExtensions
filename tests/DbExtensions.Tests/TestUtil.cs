74663349985990442e08070c770ec4f73344b4a4
Max Toro <maxtoroq@gmail.com>
2018-02-02 20:32:34 -0300

    appveyor tests connection string

--- tests/DbExtensions.Tests/TestUtil.cs
+++ tests/DbExtensions.Tests/TestUtil.cs
@@ -1,31 +1,56 @@
 ﻿using System;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.IO;
 using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
 
 namespace DbExtensions.Tests {
 
    static class TestUtil {
 
+      static bool IsAppVeyor = Environment.GetEnvironmentVariable("APPVEYOR") == "True";
+
       public static Database MySqlDatabase() {
 
-         DbConnection conn = MySql.Data.MySqlClient.MySqlClientFactory.Instance.CreateConnection();
+         DbConnection conn = MySqlClientFactory.Instance.CreateConnection();
 
          return new Database(conn);
       }
 
       public static Database SqlServerDatabase() {
 
-         DbConnection conn = System.Data.SqlClient.SqlClientFactory.Instance.CreateConnection();
-         conn.ConnectionString = @"Data Source=(localdb)\mssqllocaldb;";
+         var builder = new SqlConnectionStringBuilder();
+         builder.DataSource = @"(localdb)\mssqllocaldb";
+
+         if (IsAppVeyor) {
+            builder.DataSource = @"(local)\sql2012sp1";
+            builder.UserID = "sa";
+            builder.Password = "Password12!";
+         }
+
+         DbConnection conn = SqlClientFactory.Instance.CreateConnection();
+         conn.ConnectionString = builder.ToString();
 
          return new Database(conn);
       }
 
       public static Database SqlServerNorthwindDatabase() {
 
-         DbConnection conn = System.Data.SqlClient.SqlClientFactory.Instance.CreateConnection();
-         conn.ConnectionString = $@"Data Source=(localdb)\mssqllocaldb; AttachDbFileName={Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\samples\App\bin\Debug\Northwind\Northwind.mdf"))}; Integrated Security=true; MultipleActiveResultSets=true";
+         var builder = new SqlConnectionStringBuilder();
+         builder.DataSource = @"(localdb)\mssqllocaldb";
+         builder.AttachDBFilename = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\samples\App\bin\Debug\Northwind\Northwind.mdf"));
+         builder.IntegratedSecurity = true;
+         builder.MultipleActiveResultSets = true;
+
+         if (IsAppVeyor) {
+            builder.DataSource = @"(local)\sql2012sp1";
+            builder.UserID = "sa";
+            builder.Password = "Password12!";
+         }
+
+         DbConnection conn = SqlClientFactory.Instance.CreateConnection();
+         conn.ConnectionString = builder.ToString();
 
          return new Database(conn);
       }
