924ac583806ef5a4684e37608ebb96db7fcc5073
Max Toro <maxtoroq@gmail.com>
2025-06-16 22:19:29 -0400

    removed using

--- src/DbExtensions/Mapper.cs
+++ src/DbExtensions/Mapper.cs
@@ -16,7 +16,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
