9d4533ea76663177cd4a22f7e83096546db3d9f1
Max Toro <maxtoroq@gmail.com>
2025-08-17 15:26:12 -0400

    using string interpolation on Metadata.Error class

--- src/DbExtensions/Metadata/Error.cs
+++ src/DbExtensions/Metadata/Error.cs
@@ -19,8 +19,6 @@ using System;
 
 namespace DbExtensions.Metadata;
 
-using static String;
-
 static class Error {
 
    internal static Exception
@@ -35,32 +33,32 @@ static class Error {
 
    internal static Exception
    InvalidFieldInfo(object p0, object p1, object p2) {
-      return new ArgumentException(Format("Could not create FieldAccessor&lt;{0},{1}&gt; from FieldInfo '{2}'.", p0, p1, p2));
+      return new ArgumentException($"Could not create FieldAccessor&lt;{p0},{p1}&gt; from FieldInfo '{p2}'.");
    }
 
    internal static Exception
    CouldNotCreateAccessorToProperty(object p0, object p1, object p2) {
-      return new ArgumentException(Format("Could not create PropertyAccessor&lt;{0},{1}&gt; to {2}.", p0, p1, p2));
+      return new ArgumentException($"Could not create PropertyAccessor&lt;{p0},{p1}&gt; to {p2}.");
    }
 
    internal static Exception
    UnableToAssignValueToReadonlyProperty(object p0) {
-      return new InvalidOperationException(Format("Unable to assign value to read only property '{0}'.", p0));
+      return new InvalidOperationException($"Unable to assign value to read only property '{p0}'.");
    }
 
    internal static Exception
    NoDiscriminatorFound(object p0) {
-      return new InvalidOperationException(Format("The inheritance type '{0}' does not declare a discriminator column.", p0));
+      return new InvalidOperationException($"The inheritance type '{p0}' does not declare a discriminator column.");
    }
 
    internal static Exception
    InheritanceTypeDoesNotDeriveFromRoot(object p0, object p1) {
-      return new InvalidOperationException(Format("Inheritance type '{0}' does not derive from inheritance root type '{1}'.", p0, p1));
+      return new InvalidOperationException($"Inheritance type '{p0}' does not derive from inheritance root type '{p1}'.");
    }
 
    internal static Exception
    AbstractClassAssignInheritanceDiscriminator(object p0) {
-      return new InvalidOperationException(Format("Abstract class '{0}' should not be assigned an inheritance discriminator key.", p0));
+      return new InvalidOperationException($"Abstract class '{p0}' should not be assigned an inheritance discriminator key.");
    }
 
    internal static Exception
@@ -75,126 +73,126 @@ static class Error {
 
    internal static Exception
    InheritanceTypeHasMultipleDiscriminators(object p0) {
-      return new InvalidOperationException(Format("The inherited type '{0}' cannot have multiple discriminator key values.", p0));
+      return new InvalidOperationException($"The inherited type '{p0}' cannot have multiple discriminator key values.");
    }
 
    internal static Exception
    InheritanceCodeUsedForMultipleTypes(object p0) {
-      return new InvalidOperationException(Format("The inheritance code '{0}' is used for multiple types.", p0));
+      return new InvalidOperationException($"The inheritance code '{p0}' is used for multiple types.");
    }
 
    internal static Exception
    InheritanceTypeHasMultipleDefaults(object p0) {
-      return new InvalidOperationException(Format("The inheritance type '{0}' has multiple defaults.", p0));
+      return new InvalidOperationException($"The inheritance type '{p0}' has multiple defaults.");
    }
 
    internal static Exception
    InheritanceHierarchyDoesNotDefineDefault(object p0) {
-      return new InvalidOperationException(Format("The inheritance hierarchy rooted at '{0}' does not define a default.", p0));
+      return new InvalidOperationException($"The inheritance hierarchy rooted at '{p0}' does not define a default.");
    }
 
    internal static Exception
    InheritanceSubTypeIsAlsoRoot(object p0) {
-      return new InvalidOperationException(Format("The inheritance subtype '{0}' is also declared as a root type.", p0));
+      return new InvalidOperationException($"The inheritance subtype '{p0}' is also declared as a root type.");
    }
 
    internal static Exception
    NonInheritanceClassHasDiscriminator(object p0) {
-      return new InvalidOperationException(Format("The inheritance type '{0}' has a discriminator but is not part of a mapped inheritance hierarchy.", p0));
+      return new InvalidOperationException($"The inheritance type '{p0}' has a discriminator but is not part of a mapped inheritance hierarchy.");
    }
 
    internal static Exception
    MemberMappedMoreThanOnce(object p0) {
-      return new InvalidOperationException(Format("The member '{0}' is mapped more than once.", p0));
+      return new InvalidOperationException($"The member '{p0}' is mapped more than once.");
    }
 
    internal static Exception
    BadStorageProperty(object p0, object p1, object p2) {
-      return new InvalidOperationException(Format("Bad Storage property: '{0}' on member '{1}.{2}'.", p0, p1, p2));
+      return new InvalidOperationException($"Bad Storage property: '{p0}' on member '{p1}.{p2}'.");
    }
 
    internal static Exception
    IncorrectAutoSyncSpecification(object p0) {
-      return new InvalidOperationException(Format("Incorrect AutoSync specification for member '{0}'.", p0));
+      return new InvalidOperationException($"Incorrect AutoSync specification for member '{p0}'.");
    }
 
    internal static Exception
    BadKeyMember(object p0, object p1, object p2) {
-      return new InvalidOperationException(Format("Could not find key member '{0}' of key '{1}' on type '{2}'. The key may be wrong or the field or property on '{2}' has changed names.", p0, p1, p2));
+      return new InvalidOperationException($"Could not find key member '{p0}' of key '{p1}' on type '{p2}'. The key may be wrong or the field or property on '{p2}' has changed names.");
    }
 
    internal static Exception
    UnableToResolveRootForType(object p0) {
-      return new InvalidOperationException(Format("Mapping Problem: Unable to resolve root for type '{0}'.", p0));
+      return new InvalidOperationException($"Mapping Problem: Unable to resolve root for type '{p0}'.");
    }
 
    internal static Exception
    CouldNotFindTypeFromMapping(object p0) {
-      return new InvalidOperationException(Format("Mapping Problem: Cannot find type '{0}' from mapping.", p0));
+      return new InvalidOperationException($"Mapping Problem: Cannot find type '{p0}' from mapping.");
    }
 
    internal static Exception
    TwoMembersMarkedAsPrimaryKeyAndDBGenerated(object p0, object p1) {
-      return new InvalidOperationException(Format("Members '{0}' and '{1}' both marked as IsPrimaryKey and IsDbGenerated.", p0, p1));
+      return new InvalidOperationException($"Members '{p0}' and '{p1}' both marked as IsPrimaryKey and IsDbGenerated.");
    }
 
    internal static Exception
    TwoMembersMarkedAsRowVersion(object p0, object p1) {
-      return new InvalidOperationException(Format("Members '{0}' and '{1}' both marked as row version.", p0, p1));
+      return new InvalidOperationException($"Members '{p0}' and '{p1}' both marked as row version.");
    }
 
    internal static Exception
    TwoMembersMarkedAsInheritanceDiscriminator(object p0, object p1) {
-      return new InvalidOperationException(Format("Members '{0}' and '{1}' both marked as inheritance discriminator.", p0, p1));
+      return new InvalidOperationException($"Members '{p0}' and '{p1}' both marked as inheritance discriminator.");
    }
 
    internal static Exception
    CouldNotFindRuntimeTypeForMapping(object p0) {
-      return new InvalidOperationException(Format("Mapping Problem: Cannot find runtime type for type mapping '{0}'.", p0));
+      return new InvalidOperationException($"Mapping Problem: Cannot find runtime type for type mapping '{p0}'.");
    }
 
    internal static Exception
    UnexpectedNull(object p0) {
-      return new InvalidOperationException(Format("Unexpected null '{0}'.", p0));
+      return new InvalidOperationException($"Unexpected null '{p0}'.");
    }
 
    internal static Exception
    InvalidDeleteOnNullSpecification(object p0) {
-      return new InvalidOperationException(Format("Invalid DeleteOnNull specification for member '{0}'. DeleteOnNull can only be true for singleton association members mapped to non-nullable foreign key columns.", p0));
+      return new InvalidOperationException($"Invalid DeleteOnNull specification for member '{p0}'. DeleteOnNull can only be true for singleton association members mapped to non-nullable foreign key columns.");
    }
 
    internal static Exception
    MappedMemberHadNoCorrespondingMemberInType(object p0, object p1) {
-      return new NotSupportedException(Format("The column or association '{0}' in the mapping had no corresponding member in type '{1}'. Mapping members from above root type is not supported.", p0, p1));
+      return new NotSupportedException($"The column or association '{p0}' in the mapping had no corresponding member in type '{p1}'. Mapping members from above root type is not supported.");
    }
 
    internal static Exception
    DiscriminatorClrTypeNotSupported(object p0, object p1, object p2) {
-      return new NotSupportedException(Format("Discriminator '{0}.{1}' may not be type '{2}'.", p0, p1, p2));
+      return new NotSupportedException($"Discriminator '{p0}.{p1}' may not be type '{p2}'.");
    }
 
    internal static Exception
    IdentityClrTypeNotSupported(object p0, object p1, object p2) {
-      return new NotSupportedException(Format("Invalid type mapping for Identity member '{0}.{1}'.  Type '{2}' is not supported for identity members.", p0, p1, p2));
+      return new NotSupportedException($"Invalid type mapping for Identity member '{p0}.{p1}'. Type '{p2}' is not supported for identity members.");
    }
 
    internal static Exception
    PrimaryKeyInSubTypeNotSupported(object p0, object p1) {
-      return new NotSupportedException(Format("The subtype '{0}' cannot contain the primary key member '{1}'.", p0, p1));
+      return new NotSupportedException($"The subtype '{p0}' cannot contain the primary key member '{p1}'.");
    }
 
    internal static Exception
    MismatchedThisKeyOtherKey(object p0, object p1) {
-      return new InvalidOperationException(Format("The number of ThisKey columns is different from the number of OtherKey columns for the association property '{0}' in the type '{1}'.", p0, p1));
+      return new InvalidOperationException($"The number of ThisKey columns is different from the number of OtherKey columns for the association property '{p0}' in the type '{p1}'.");
    }
 
    internal static Exception
    MappingOfInterfacesMemberIsNotSupported(object p0, object p1) {
-      return new NotSupportedException(Format("The mapping of interface member {0}.{1} is not supported.", p0, p1));
+      return new NotSupportedException($"The mapping of interface member {p0}.{p1} is not supported.");
    }
 
    internal static Exception
    UnmappedClassMember(object p0, object p1) {
-      return new InvalidOperationException(Format("Class member {0}.{1} is unmapped.", p0, p1));
+      return new InvalidOperationException($"Class member {p0}.{p1} is unmapped.");
    }
 }
