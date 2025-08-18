7e045da23e4ec05c9b7fbaa0b1178483b07eee72
Max Toro <maxtoroq@gmail.com>
2025-08-17 22:17:04 -0400

    TestUtil tweaks and run on .net 8

--- tests/DbExtensions.Tests/TestUtil.cs
+++ tests/DbExtensions.Tests/TestUtil.cs
@@ -11,10 +11,12 @@ namespace DbExtensions.Tests;
 
 static class TestUtil {
 
-   public static Database MockDatabase(bool useCompiledMapping, string providerInvariantName = "MySql.Data.MySqlClient") =>
+   public static Database
+   MockDatabase(bool useCompiledMapping, string providerInvariantName = "MySql.Data.MySqlClient") =>
       MockDatabaseImpl(useCompiledMapping, providerInvariantName).Object;
 
-   public static Mock<Database> MockDatabaseImpl(bool useCompiledMapping, string providerInvariantName) {
+   public static Mock<Database>
+   MockDatabaseImpl(bool useCompiledMapping, string providerInvariantName) {
 
       var mockConn = new Mock<IDbConnection>();
 
@@ -27,7 +29,8 @@ static class TestUtil {
       return mockDb;
    }
 
-   public static Database MockQuery(bool useCompiledMapping, params IEnumerable<KeyValuePair<string, object>>[] data) {
+   public static Database
+   MockQuery(bool useCompiledMapping, params IEnumerable<KeyValuePair<string, object>>[] data) {
 
       var reader = new TestDataReader(data
          .Select(p => p as KeyValuePair<string, object>[]
@@ -41,7 +44,8 @@ static class TestUtil {
       return mockDb.Object;
    }
 
-   public static void SetupReader(Mock<Database> mockDb, IDataReader reader, string commandText = null) {
+   public static void
+   SetupReader(Mock<Database> mockDb, IDataReader reader, string commandText = null) {
 
       mockDb.Setup(db => db.CreateCommand(It.IsAny<string>(), It.IsAny<object[]>()))
          .Returns(() => {
@@ -54,7 +58,8 @@ static class TestUtil {
          });
    }
 
-   public static Database RealDatabase(bool useCompiledMapping) {
+   public static Database
+   RealDatabase(bool useCompiledMapping) {
 
       var builder = new SQLiteConnectionStringBuilder {
          DataSource = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\samples\App\bin\Debug\net8.0\Northwind\Northwind.sl3")),
@@ -73,113 +78,153 @@ static class TestUtil {
       return db;
    }
 
-   public static bool SqlEquals(SqlSet set, SqlBuilder query) =>
+   public static bool
+   SqlEquals(SqlSet set, SqlBuilder query) =>
       String.Equals(Regex.Replace(set.ToString(), "dbex_set[0-9]+", "_"), query.ToString(), StringComparison.Ordinal);
 }
 
 class TestDataReader : IDataReader {
 
-   readonly KeyValuePair<string, object>[][] _data;
-   KeyValuePair<string, object>[] _row;
-   int _rowIndex;
+   readonly KeyValuePair<string, object>[][]
+   _data;
 
-   public object this[int i] => _row[i].Value;
+   KeyValuePair<string, object>[]
+   _row;
 
-   public object this[string name] => _row[GetOrdinal(name)].Value;
+   int
+   _rowIndex;
 
-   public int Depth => throw new NotImplementedException();
+   public object
+   this[int i] => _row[i].Value;
 
-   public bool IsClosed => false;
+   public object
+   this[string name] => _row[GetOrdinal(name)].Value;
 
-   public int RecordsAffected => -1;
+   public int
+   Depth => throw new NotImplementedException();
 
-   public int FieldCount => _row.Length;
+   public bool
+   IsClosed => false;
 
-   public TestDataReader(params KeyValuePair<string, object>[][] data) {
+   public int
+   RecordsAffected => -1;
+
+   public int
+   FieldCount => _row.Length;
+
+   public
+   TestDataReader(params KeyValuePair<string, object>[][] data) {
       _data = data;
    }
 
-   public void Close() { }
+   public void
+   Close() { }
 
-   public void Dispose() { }
+   public void
+   Dispose() { }
 
-   public bool GetBoolean(int i) =>
+   public bool
+   GetBoolean(int i) =>
       (bool)this[i];
 
-   public byte GetByte(int i) =>
+   public byte
+   GetByte(int i) =>
       (byte)this[i];
 
-   public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) =>
+   public long
+   GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) =>
       throw new NotImplementedException();
 
-   public char GetChar(int i) =>
+   public char
+   GetChar(int i) =>
       (char)this[i];
 
-   public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) =>
+   public long
+   GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) =>
       throw new NotImplementedException();
 
-   public IDataReader GetData(int i) =>
+   public IDataReader
+   GetData(int i) =>
       throw new NotImplementedException();
 
-   public string GetDataTypeName(int i) =>
+   public string
+   GetDataTypeName(int i) =>
       throw new NotImplementedException();
 
-   public DateTime GetDateTime(int i) =>
+   public DateTime
+   GetDateTime(int i) =>
       (DateTime)this[i];
 
-   public decimal GetDecimal(int i) =>
+   public decimal
+   GetDecimal(int i) =>
       (decimal)this[i];
 
-   public double GetDouble(int i) =>
+   public double
+   GetDouble(int i) =>
       (double)this[i];
 
-   public Type GetFieldType(int i) =>
+   public Type
+   GetFieldType(int i) =>
       throw new NotImplementedException();
 
-   public float GetFloat(int i) =>
+   public float
+   GetFloat(int i) =>
       (float)this[i];
 
-   public Guid GetGuid(int i) =>
+   public Guid
+   GetGuid(int i) =>
       (Guid)this[i];
 
-   public short GetInt16(int i) =>
+   public short
+   GetInt16(int i) =>
       (short)this[i];
 
-   public int GetInt32(int i) =>
+   public int
+   GetInt32(int i) =>
       (int)this[i];
 
-   public long GetInt64(int i) =>
+   public long
+   GetInt64(int i) =>
       (long)this[i];
 
-   public string GetName(int i) =>
+   public string
+   GetName(int i) =>
       _row[i].Key;
 
-   public int GetOrdinal(string name) =>
+   public int
+   GetOrdinal(string name) =>
       _row.Select((p, i) => new { p, i })
          .Where(p => p.p.Key == name)
          .Select(p => p.i)
          .DefaultIfEmpty(-1)
          .First();
 
-   public DataTable GetSchemaTable() =>
+   public DataTable
+   GetSchemaTable() =>
       throw new NotImplementedException();
 
-   public string GetString(int i) =>
+   public string
+   GetString(int i) =>
       (string)this[i];
 
-   public object GetValue(int i) =>
+   public object
+   GetValue(int i) =>
       this[i];
 
-   public int GetValues(object[] values) =>
+   public int
+   GetValues(object[] values) =>
       throw new NotImplementedException();
 
-   public bool IsDBNull(int i) =>
+   public bool
+   IsDBNull(int i) =>
       this[i] is null;
 
-   public bool NextResult() =>
+   public bool
+   NextResult() =>
       throw new NotImplementedException();
 
-   public bool Read() {
+   public bool
+   Read() {
 
       if (_data.Length > _rowIndex) {
          _row = _data[_rowIndex];
@@ -190,7 +235,8 @@ class TestDataReader : IDataReader {
       return false;
    }
 
-   public void Reset() {
+   public void
+   Reset() {
       _row = null;
       _rowIndex = 0;
    }
