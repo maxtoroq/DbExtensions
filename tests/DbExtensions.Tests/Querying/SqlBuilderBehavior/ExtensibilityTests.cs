509fbc094cb3f47fd64c7cf26642ab32f966eb43
Max Toro <maxtoroq@gmail.com>
2025-10-10 13:45:14 -0300

    SqlBuilder extension clause test

---
+++ tests/DbExtensions.Tests/Querying/SqlBuilderBehavior/ExtensibilityTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+using NUnit.Framework;
+
+namespace DbExtensions.Tests.Querying.SqlBuilderBehavior {
+
+   [TestFixture]
+   public class ExtensibilityTests {
+
+      [Test]
+      public void Extension_Clause() {
+
+         var query = ((SqlBuilder)$"""
+            SELECT ProductID, ProductName
+            FROM Products
+            OFFSET {10} ROWS
+            """)
+            .FETCH($"NEXT {5} ROWS ONLY");
+
+         Assert.AreEqual("""
+            SELECT ProductID, ProductName
+            FROM Products
+            OFFSET {0} ROWS
+            FETCH NEXT {1} ROWS ONLY
+            """, query.ToString());
+
+         Assert.IsTrue(query.CurrentClause is SqlBuilderExtensions.FetchClause);
+      }
+   }
+
+   public static class SqlBuilderExtensions {
+
+      public sealed record class FetchClause() : SqlClause("FETCH", null);
+
+      public static SqlBuilder FETCH(this SqlBuilder sql, [InterpolatedStringHandlerArgument(nameof(sql))] ref SqlBuilder.ClauseStringHandler<FetchClause> handler) {
+         return sql;
+      }
+
+      public static SqlBuilder FETCH(this SqlBuilder sql, string text) {
+         return sql.AppendClause<FetchClause>()
+            .Append(text);
+      }
+   }
+}
